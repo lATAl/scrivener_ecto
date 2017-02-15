70d15c431c59eb938e1d0f9bbdaddadd3b947c73
Lê Anh Tuấn <latal@users.noreply.github.com>
2017-02-15 17:24:08 +0700

    Try fix entries missing when join

--- lib/scrivener/paginater/ecto/query.ex
+++ lib/scrivener/paginater/ecto/query.ex
@@ -18,13 +18,40 @@ defimpl Scrivener.Paginater, for: Ecto.Query do
     }
   end
 
-  defp entries(query, repo, page_number, page_size) do
-    offset = page_size * (page_number - 1)
-
-    query
-    |> limit(^page_size)
-    |> offset(^offset)
-    |> repo.all
+  defp entries(query, repo, page_number, page_size) do    
+    offset = page_size * (page_number - 1)    
+    
+    if joins?(query) do   
+      ids = query   
+      |> remove_clauses   
+      |> select([x], {x.id})    
+      |> group_by([x], x.id)    
+      |> offset([_], ^offset)   
+      |> limit([_], ^page_size)   
+      |> repo.all   
+      |> Enum.map(&elem(&1, 0))   
+    
+      query   
+      |> where([x], x.id in ^ids)   
+      |> distinct(true)   
+      |> repo.all   
+    else    
+      query   
+      |> limit([_], ^page_size)   
+      |> offset([_], ^offset)   
+      |> repo.all   
+    end   
+  end   
+    
+  defp joins?(query) do   
+    Enum.count(query.joins) > 0   
+  end   
+    
+  defp remove_clauses(query) do   
+    query   
+    |> exclude(:preload)    
+    |> exclude(:select)   
+    |> exclude(:group_by)   
   end
 
   defp total_entries(query, repo) do
